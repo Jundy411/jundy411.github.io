26813043f5d08279d6121dc4008c394ca7bd4816
Jundy Islami <jundyislami4@gmail.com>
2022-07-21 11:27:21 +0700

    Menyalin isi folder build

---
+++ precache-manifest.273423ea16712492ac335cca3150689c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f5f183f0044a9e1658a",
+    "url": "/static/css/main.25e35da4.chunk.css"
+  },
+  {
+    "revision": "3f5f183f0044a9e1658a",
+    "url": "/static/js/main.3f5f183f.chunk.js"
+  },
+  {
+    "revision": "4eddf295c0fa435b8827",
+    "url": "/static/js/1.4eddf295.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "161ac22784eb0b08449f4556de3f6eea",
+    "url": "/index.html"
+  }
+];
